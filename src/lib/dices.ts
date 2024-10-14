16819748880631af903c2019f5869a6aa8dd5ce4
Simon Bergot <sbergot@cegid.com>
2024-10-14 13:29:59 +0200

    fix ability rolling

--- src/lib/dices.ts
+++ src/lib/dices.ts
@@ -1,38 +1,49 @@
-export interface RollTable {
-  "1": string;
-  "20": string;
-  "11-19": string;
-  "6-10": string;
-  "2-5": string;
-}
+export type RollTable = Record<string, string>;
 
 export interface RollResult {
   value: number;
   text: string;
 }
 
+type Range = number[];
+
+function parseRange(strRange: string): Range {
+  if (/^\d+$/.test(strRange)) {
+    return [parseInt(strRange)]
+  }
+
+  const match = /(\d+)-(\d+)/.exec(strRange);
+  if (!match) {
+    throw new Error(`invalid range: ${strRange}`);
+  }
+  const lower = parseInt(match[1]);
+  const higher = parseInt(match[2]);
+  const result = [];
+  for (let i = lower; i <= higher; i++) {
+    result.push(i);
+  }
+  return result;
+}
+
+function matchRange(range: Range, value: number) {
+  return range.includes(value);
+}
+
 function simpleRoll(sides: number): number {
   return Math.floor(Math.random() * sides) + 1;
 }
 
-function matchValue(value: number): keyof RollTable {
-  if (value === 1) {
-    return "1";
+function matchValue(table: RollTable, value: number): string {
+  for (let strRange in table) {
+    if (matchRange(parseRange(strRange), value)) {
+      return table[strRange];
+    }
   }
-  if (value < 6) {
-    return "2-5";
-  }
-  if (value < 11) {
-    return "6-10";
-  }
-  if (value < 20) {
-    return "11-19";
-  }
-  return "20";
+  throw new Error(`could not match value: ${value}`);
 }
 
 export function roll(table: RollTable): RollResult {
   const value = simpleRoll(20);
-  const text = table[matchValue(value)];
+  const text = matchValue(table, value);
   return { value, text };
 }
--- todo.md
+++ todo.md
@@ -1,5 +1,2 @@
-* improve pilots
-  * add roll tables for all skills
-
 * roller sub page
 * mech sheet
